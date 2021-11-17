077a2681618882b9ca77ead5b46d079852199e34
luv4boot <luv4boot@gmail.com>
2021-11-17 11:35:21 +0530

    adding book controller

---
+++ src/main/java/com/luv4code/sf/controller/BookController.java
@@ -0,0 +1,21 @@
+package com.luv4code.sf.controller;
+
+import com.luv4code.sf.repositories.BookRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class BookController {
+    private final BookRepository bookRepository;
+
+    public BookController(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    @GetMapping("/books")
+    public String getBooks(Model model) {
+        model.addAttribute("books", bookRepository.findAll());
+        return "books";
+    }
+}
